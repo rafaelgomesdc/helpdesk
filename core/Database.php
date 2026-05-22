fc45cc73720bb9c15b7d5879738379250328fe3e
rafaelgomesdc <rafaelgomesdc0000@gmail.com>
2026-05-22 09:16:29 -0300

    chore: arquivos para conexao do db

---
+++ core/Database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database{
+    private $host;
+    private $dbname;
+    private $user;
+    private $pass;
+    private $conn;
+
+    public function __contruct(){
+        //
+    }
+
+    public function connect(){
+        //
+        
+        return $this->conn;
+    }
+}
+
+?>
