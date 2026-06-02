5fc744d04bd4dc31c68a9bd489b4fd754af28555
Yuuuudz <viudyisawa@gmail.com>
2026-06-02 10:00:51 -0300

    feat: adiciona views do painel técnico

---
+++ helpdesk/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TechnicianController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::prefix('tecnico')->name('technician.')->group(function () {
+    Route::get('/em-atendimento',          [TechnicianController::class, 'inProgress'])->name('in-progress');
+    Route::post('/chamados/{id}/atribuir', [TechnicianController::class, 'assign'])->name('assign');
+    Route::get('/chamados/{id}/solucao',   [TechnicianController::class, 'solutionForm'])->name('solution');
+    Route::post('/chamados/{id}/solucao',  [TechnicianController::class, 'saveSolution'])->name('save-solution');
+});
